d8bf41a8c64654c2d9133334a4b029f9ca77c45d
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2020-05-23 11:44:58 +0200

    Use Azure Pipelines stages

--- build/generate_docs.ps1
+++ build/generate_docs.ps1
@@ -8,7 +8,7 @@ $docfx = Join-Path (Join-Path $docfxRoot "tools") "docfx.exe"
 if (-not (Test-Path $docfx)) {
     mkdir -p $docfxRoot -ErrorAction Ignore | Out-Null
     $temp = (New-TemporaryFile).FullName + ".zip"
-    Invoke-WebRequest "https://www.nuget.org/api/v2/package/docfx.console/2.42.0" -O $temp
+    Invoke-WebRequest "https://www.nuget.org/api/v2/package/docfx.console/2.52.0" -O $temp
     Expand-Archive $temp -DestinationPath $docfxRoot
     Remove-Item $temp
 }
